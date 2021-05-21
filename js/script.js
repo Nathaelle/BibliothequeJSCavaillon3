a0337f42b09806e2cc378b1dfa814128bced338d
Nathaelle <scriptales.clients@gmail.com>
2021-05-21 10:57:11 +0200

    Exercice 5 : Ajout de deux autres livres

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ let livre1 = {
     titre: "Les Misérables",
     auteur: "Victor Hugo"
 }
+
+let livre2 = {
+    reference: "nb548",
+    titre: "L'Iliade et l'Odyssée",
+    auteur: "Homère"
+}
+
+let livre3 = {
+    reference: "ut862",
+    titre: "1984",
+    auteur: "George Orwell"
+}
+
+let livres = [livre1, livre2, livre3];
